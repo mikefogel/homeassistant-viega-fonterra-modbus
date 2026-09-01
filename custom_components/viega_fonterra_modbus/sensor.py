c970cb9a1a4b693724a8cf990f63c89caeed1f52
mikefogel <148653533+mikefogel@users.noreply.github.com>
2026-09-01 22:07:27 +0200

    Add ViegaDummySensor to custom components

--- custom_components/viega_fonterra_modbus/sensor.py
+++ custom_components/viega_fonterra_modbus/sensor.py
@@ -1,1 +1,15 @@
+from homeassistant.components.sensor import SensorEntity
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    async_add_entities([ViegaDummySensor()])
+
+class ViegaDummySensor(SensorEntity):
+    @property
+    def name(self):
+        return "Viega Dummy Sensor"
+
+    @property
+    def state(self):
+        return 42
 
