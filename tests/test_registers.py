408c6902c26588cd3768cc548ed30b9f27f5cc9c
Michael Fogel <michael_fogel@web.de>
2026-09-03 22:27:15 +0200

    test erfolgreich

--- tests/test_registers.py
+++ tests/test_registers.py
@@ -87,4 +87,4 @@ def test_describe_error_code_reports_unknown_for_undocumented_codes():
 
 
 def test_decode_text_registers_uses_little_endian_ascii_and_strips_padding():
-    assert decode_text_registers([0x5649, 0x4547, 0x4100, 0x2020]) == "VIEGA"
+    assert decode_text_registers([0x4956, 0x4745, 0x0041, 0x2020]) == "VIEGA"
